c798e2333f798256a65637f2cfede74ce5f568c8
sehjang <74850752+wkdtpgns5016@users.noreply.github.com>
2022-12-06 14:33:14 +0900

    Feat: background 변경 (#44)
    1. 무늬가 보이지 않도록 배경색 수정
    
    resolved #43

--- executing/executing.c
+++ executing/executing.c
@@ -22,43 +22,63 @@ void	init_background(t_game *game)
 	bg.horizon = WIN_H / (2 * bg.bytes_per_pixel);
 	game->bg = bg;
 }
-
 void	paint_background(t_game *game)
 {
-	t_bg	bg;
-	int		*memory;
 	int		x;
 	int		y;
-	int		idx;
 
 	x = 0;
 	y = 0;
-	bg = game->bg;
-	memory = (int *)bg.img_memory;
-	while (y <= bg.horizon)
+	while (y < WIN_H)
 	{
-		idx = bg.bytes_per_pixel * x + bg.size_line * y;
-		memory[idx] = game->color[0];
-		idx = bg.bytes_per_pixel * x + bg.size_line * \
-			  (bg.horizon * 2 - y);
-		memory[idx] = game->color[1];
-		if (bg.bytes_per_pixel * x == bg.size_line)
+		x = 0;
+		while (x < WIN_W)
 		{
-			x = 0;
-			y++;
-		}
-		else 
+			if (y < WIN_H / 2)
+				game->bg.memory[y * WIN_W + x] = game->color[0];
+			else
+				game->bg.memory[y * WIN_W + x] = game->color[1];
 			x++;
+		}
+		y++;
 	}
 }
+
+// void	paint_background(t_game *game)
+// {
+// 	t_bg	bg;
+// 	int		*memory;
+// 	int		x;
+// 	int		y;
+// 	int		idx;
+
+// 	x = 0;
+// 	y = 0;
+// 	bg = game->bg;
+// 	memory = (int *)bg.img_memory;
+// 	while (y <= bg.horizon)
+// 	{
+// 		idx = bg.bytes_per_pixel * x + bg.size_line * y;
+// 		memory[idx] = game->color[0];
+// 		idx = bg.bytes_per_pixel * x + bg.size_line * \
+// 			  (bg.horizon * 2 - y);
+// 		memory[idx] = game->color[1];
+// 		if (bg.bytes_per_pixel * x == bg.size_line)
+// 		{
+// 			x = 0;
+// 			y++;
+// 		}
+// 		else 
+// 			x++;
+// 	}
+// }
 
 void	executing(t_game *game)
 {
 	init_window(game);
 	init_background(game);
-	//paint_background(game);
+	paint_background(game);
 	locate_man(game);
-	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->bg.img_ptr, 0, 0);
 	raycasting(game, &game->man, WIN_W);
 	mlx_put_image_to_window(game->mlx_ptr, game->win_ptr, game->bg.img_ptr, 0, 0);
 	mlx_hook(game->win_ptr, X_EVENT_KEY_PRESS, 0, &key_press, game);
